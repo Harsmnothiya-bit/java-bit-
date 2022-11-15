e483dc2cd9d16a87961a8156d9cd162316ceaa4e
Harsh Mnothiya <114900961+Harsmnothiya-bit@users.noreply.github.com>
2022-11-15 16:26:24 +0530

    java code

---
+++ While2.java
@@ -0,0 +1,10 @@
+public class While2 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 12) {
+            System.out.println(i);
+            i = i * 1;
+        }
+    }
+
+}
---
+++ loopjabed.java
@@ -0,0 +1,6 @@
+public class loopjabed {
+    public static void main(String[] argz) {
+        for (int i = 0; i < 78; i++)
+            System.out.println(i);
+    }
+}
